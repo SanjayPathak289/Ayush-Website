42b9393a997a390169d2f601df5c45999f2a8fff
Sanjay Pathak <forjoyonly289@gmail.com>
2025-02-06 23:19:46 +0530

    push

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const { job } = require("./cron.js");
+dotenv.config();
+const app = express();
+app.use(express.json());
+const { google } = require("googleapis");
+const OAuth2 = google.auth.OAuth2;
+job.start();
+// app.get("/contact", (req, res) => {
+//     res.status(200).send("HII");
+// })
+app.post("/contact", (req, res) => {
+    const createTransporter = async () => {
+        const oauth2Client = new OAuth2(
+            process.env.CLIENT_ID,
+            process.env.CLIENT_SECRET,
+            "https://developers.google.com/oauthplayground"
+        );
+        oauth2Client.setCredentials({
+            refresh_token: process.env.REFRESH_TOKEN
+        });
+        const accessToken = await new Promise((resolve, reject) => {
+            oauth2Client.getAccessToken((err, token) => {
+                if (err) {
+                    console.log(err);
+                }
+                resolve(token);
+            });
+        });
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: process.env.EMAIL,
+                accessToken,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN
+            },
+            tls: {
+                rejectUnauthorized: false
+            }
+        });
+        return transporter;
+
+    };
+    const sendmail = async (mailOptions) => {
+        let emailTransporter = await createTransporter();
+        await emailTransporter.sendMail(mailOptions);
+        res.status(200).send("Email sent");
+    }
+
+    sendmail({
+        subject: `Message from a website visitor - ${req.body.name}`,
+        text: `${req.body.email}` + `\n` + req.body.message,
+        from: req.body.email,
+        to: process.env.EMAIL,
+    })
+})
+app.listen(3000, () => {
+    console.log("Server started running!");
+})
